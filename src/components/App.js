13980471dc8c8179a2293a614334d746bbefdec7
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-12-01 21:26:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import {useState} from "react";
 const App = () => {
+  const[count, setCount]=useState(0);
+  function hello(){
+    setCount(count+1);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p id='t'>Button clicked {count} times</p>
+        <button onClick={hello}>Click me</button>
     </div>
   )
 }
